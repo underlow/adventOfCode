39409d174652ce30960153cd5eeaccbae03514f1
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2022-12-25 12:05:31 +0100

    days 24 - part 1

--- src/test/kotlin/me/underlow/advent2022/day24/AirNumberTest.kt
+++ src/test/kotlin/me/underlow/advent2022/day24/AirNumberTest.kt
@@ -1,6 +1,6 @@
 package me.underlow.advent2022.day24
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class AirNumberTest {
@@ -21,11 +21,55 @@ class AirNumberTest {
             "1=" to 3,
             "122" to 37
         )
-        list.forEach { (from ,to) ->
-            assertEquals(to, AirNumber(from).toDecimal())
+        list.forEach { (from, to) ->
+            assertEquals(to, AirNumber.fromString(from).toDecimal())
 
         }
     }
 
+    @Test
+    fun `test sum of two AirNumbers`() {
+        data class TestData(val l: String, val r: String, val expected: Int)
 
+        val list = listOf(
+            TestData("112", "12", 39),
+            TestData("1=-0-2", "1=-0-2", 2 * 1747),
+            TestData("1=", "1=", 2 * 3),
+            TestData("2", "2", 4),
+        )
+        list.forEach { td ->
+            assertEquals(td.expected, (AirNumber.fromString(td.l) + AirNumber.fromString(td.r)).toDecimal())
+        }
+    }
+
+    @Test
+    fun `check test data step by step`() {
+        val numbers = input.split("\n").map { AirNumber.fromString(it) }
+        var acc = AirNumber.fromString("0")
+        var accDec = 0
+        for (i in numbers.indices) {
+            acc = acc + numbers[i]
+            accDec = accDec + numbers[i].toDecimal()
+            assertEquals(acc.toDecimal(), accDec, "Fail to add $numbers[i]")
+        }
+        assertEquals(4890, accDec)
+        assertEquals(4890, acc.toDecimal())
+    }
 }
+
+private val input = """
+    1=-0-2
+    12111
+    2=0=
+    21
+    2=01
+    111
+    20012
+    112
+    1=-1=
+    1-12
+    12
+    1=
+    122
+""".trimIndent()
+
