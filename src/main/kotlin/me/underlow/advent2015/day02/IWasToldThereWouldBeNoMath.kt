d7e70c3c0e6ef2186d3764f28651e3e663f5b15c
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2022-12-26 11:38:00 +0100

    2015 - day 02

--- src/main/kotlin/me/underlow/advent2015/day02/IWasToldThereWouldBeNoMath.kt
+++ src/main/kotlin/me/underlow/advent2015/day02/IWasToldThereWouldBeNoMath.kt
@@ -8,18 +8,11 @@ object IWasToldThereWouldBeNoMath {
     data class Box(val l: Int, val w: Int, val h: Int)
 
     private fun Box.requiredPaper() = 2 * l * w + 2 * w * h + 2 * h * l + listOf(l * w, w * h, h * l).min()
+    private fun Box.requiredRibbon() = 2 * listOf(l + w, w + h, h + l).min() + l * w * h
 
-    fun part1(list: List<String>): Int {
-        val boxes = parseInput(list)
-        return boxes.sumOf {
-            it.requiredPaper()
-        }
-    }
+    fun part1(list: List<String>): Int = parseInput(list).sumOf { it.requiredPaper() }
 
-    fun part2(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
-    }
+    fun part2(list: List<String>): Int = parseInput(list).sumOf { it.requiredRibbon() }
 
     //19x24x29
     private fun parseInput(list: List<String>): List<Box> = list.map {
@@ -35,7 +28,7 @@ fun main() {
     val res2 = IWasToldThereWouldBeNoMath.part2(input)
 
     checkResult(res1, 1598415)
-    checkResult(res2, 0)
+    checkResult(res2, 3812909)
 
     println(res1)
     println(res2)
