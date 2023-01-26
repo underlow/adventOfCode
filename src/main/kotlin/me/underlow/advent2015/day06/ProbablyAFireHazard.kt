888b2ea8ac924b055d8d922da444672b6349edf8
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2023-01-26 21:52:27 +0100

    2015/day 06 - ProbablyAFireHazard - part 2

--- src/main/kotlin/me/underlow/advent2015/day06/ProbablyAFireHazard.kt
+++ src/main/kotlin/me/underlow/advent2015/day06/ProbablyAFireHazard.kt
@@ -39,6 +39,39 @@ object ProbablyAFireHazard {
         }
     }
 
+
+    class Field2 {
+        private val lights = Array(1000) { Array(1000) { 0 } }
+
+        fun turnOn(square: Square) {
+            for (x in square.x1..square.x2) {
+                for (y in square.y1..square.y2) {
+                    lights[x][y] += 1
+                }
+            }
+        }
+
+        fun turnOff(square: Square) {
+            for (x in square.x1..square.x2) {
+                for (y in square.y1..square.y2) {
+                    lights[x][y] = maxOf(0, lights[x][y] - 1)
+                }
+            }
+        }
+
+        fun toggle(square: Square) {
+            for (x in square.x1..square.x2) {
+                for (y in square.y1..square.y2) {
+                    lights[x][y] +=  2
+                }
+            }
+        }
+
+        fun countOn(): Int {
+            return lights.sumOf { it.sumOf { it } }
+        }
+    }
+
     fun part1(list: List<String>): Int {
         val commands = parseInput(list)
         val field = Field()
@@ -54,8 +87,17 @@ object ProbablyAFireHazard {
     }
 
     fun part2(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
+        val commands = parseInput(list)
+        val field = Field2()
+        commands.forEach {
+            when (it.action) {
+                "on" -> field.turnOn(it)
+                "off" -> field.turnOff(it)
+                "toggle" -> field.toggle(it)
+            }
+        }
+
+        return field.countOn()
     }
 
     private fun parseInput(list: List<String>): List<Square> {
@@ -65,7 +107,7 @@ object ProbablyAFireHazard {
         // toggle 50,472 through 452,788
 
         return list.map {
-            val s = it.replace("through", ",").filter{ it.isDigit() || it == ',' }
+            val s = it.replace("through", ",").filter { it.isDigit() || it == ',' }
 
             val (x1, y1, x2, y2) = s.split(",")
 
@@ -84,8 +126,8 @@ fun main() {
     val res1 = ProbablyAFireHazard.part1(input)
     val res2 = ProbablyAFireHazard.part2(input)
 
-    checkResult(res1, 0)
-    checkResult(res2, 0)
+    checkResult(res1, 543903)
+    checkResult(res2, 14687245)
 
     println(res1)
     println(res2)
