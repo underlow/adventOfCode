c76eed632868fad7ac3f34257fc20c77bd332eeb
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2023-01-26 21:47:36 +0100

    2015/day 06 - ProbablyAFireHazard - part 1

---
+++ src/main/kotlin/me/underlow/advent2015/day06/ProbablyAFireHazard.kt
@@ -0,0 +1,92 @@
+package me.underlow.advent2015.day06
+
+import me.underlow.advent2022.checkResult
+import me.underlow.advent2022.getResourceAsLines
+
+object ProbablyAFireHazard {
+
+    data class Square(val x1: Int, val y1: Int, val x2: Int, val y2: Int, val action: String)
+
+    class Field {
+        private val lights = Array(1000) { BooleanArray(1000) }
+
+        fun turnOn(square: Square) {
+            for (x in square.x1..square.x2) {
+                for (y in square.y1..square.y2) {
+                    lights[x][y] = true
+                }
+            }
+        }
+
+        fun turnOff(square: Square) {
+            for (x in square.x1..square.x2) {
+                for (y in square.y1..square.y2) {
+                    lights[x][y] = false
+                }
+            }
+        }
+
+        fun toggle(square: Square) {
+            for (x in square.x1..square.x2) {
+                for (y in square.y1..square.y2) {
+                    lights[x][y] = !lights[x][y]
+                }
+            }
+        }
+
+        fun countOn(): Int {
+            return lights.sumOf { it.count { it } }
+        }
+    }
+
+    fun part1(list: List<String>): Int {
+        val commands = parseInput(list)
+        val field = Field()
+        commands.forEach {
+            when (it.action) {
+                "on" -> field.turnOn(it)
+                "off" -> field.turnOff(it)
+                "toggle" -> field.toggle(it)
+            }
+        }
+
+        return field.countOn()
+    }
+
+    fun part2(list: List<String>): Int {
+        val directions = parseInput(list)
+        return 0
+    }
+
+    private fun parseInput(list: List<String>): List<Square> {
+        //parse
+        // turn on 222,12 through 856,241
+        // turn off 209,780 through 572,894
+        // toggle 50,472 through 452,788
+
+        return list.map {
+            val s = it.replace("through", ",").filter{ it.isDigit() || it == ',' }
+
+            val (x1, y1, x2, y2) = s.split(",")
+
+            when {
+                it.startsWith("turn on") -> Square(x1.toInt(), y1.toInt(), x2.toInt(), y2.toInt(), "on")
+                it.startsWith("turn off") -> Square(x1.toInt(), y1.toInt(), x2.toInt(), y2.toInt(), "off")
+                else -> Square(x1.toInt(), y1.toInt(), x2.toInt(), y2.toInt(), "toggle")
+            }
+        }
+    }
+}
+
+
+fun main() {
+    val input = getResourceAsLines("/2015/06 - ProbablyAFireHazard.txt")
+    val res1 = ProbablyAFireHazard.part1(input)
+    val res2 = ProbablyAFireHazard.part2(input)
+
+    checkResult(res1, 0)
+    checkResult(res2, 0)
+
+    println(res1)
+    println(res2)
+}
---
+++ src/test/kotlin/me/underlow/advent2015/day06/ProbablyAFireHazardTest.kt
@@ -0,0 +1,23 @@
+package me.underlow.advent2015.day06
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ProbablyAFireHazardTest{
+    @Test
+    fun testPart1() {
+        val result = ProbablyAFireHazard.part1(input.split("\n"))
+        assertEquals(998996, result)
+    }
+
+    @Test
+    fun testPart2() {
+        val result = ProbablyAFireHazard.part2(input.split("\n"))
+        assertEquals(0, result)
+    }
+}
+private val input = """
+    turn on 0,0 through 999,999
+    toggle 0,0 through 999,0
+    turn off 499,499 through 500,500
+""".trimIndent()
