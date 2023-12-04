a60a194ce7f7dff12b531fa914aca4e6eeaa22e7
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2023-12-04 10:49:54 +0100

    2023/day 04 - part 1

--- src/main/kotlin/me/underlow/advent2023/Day04.kt
+++ src/main/kotlin/me/underlow/advent2023/Day04.kt
@@ -2,12 +2,22 @@ package me.underlow.advent2023
 
 import me.underlow.advent2022.checkResult
 import me.underlow.advent2022.readInput
+import java.lang.Math.pow
 
 object Scratchcards {
 
+    data class Card(val winning: List<Int>, val num: List<Int>) {
+        fun p1(): Int {
+            val total = num.filter { it in winning }.count()
+            return pow(2.0, total.toDouble() - 1).toInt()
+        }
+    }
+
     fun part1(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
+        val cards = parseInput(list)
+
+
+        return cards.map { it.p1() }.sum()
     }
 
     fun part2(list: List<String>): Int {
@@ -15,8 +25,13 @@ object Scratchcards {
         return 0
     }
 
-    private fun parseInput(list: List<String>): Any {
-        return 0
+    private fun parseInput(list: List<String>): List<Card> {
+        return list.map {
+            val skipNum = it.substringAfter(":")
+            val winning = skipNum.substringBefore("|").trim().split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+            val num = skipNum.substringAfter("|").trim().split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+            return@map Card(winning, num)
+        }
     }
 }
 
