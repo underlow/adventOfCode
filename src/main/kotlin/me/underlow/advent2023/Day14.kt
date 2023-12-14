08b84fab43ce183bcf71a0449f439f255636146a
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2023-12-14 15:51:43 +0100

    2023/day 14 - part 1

--- src/main/kotlin/me/underlow/advent2023/Day14.kt
+++ src/main/kotlin/me/underlow/advent2023/Day14.kt
@@ -6,17 +6,55 @@ import me.underlow.advent2022.readInput
 object ParabolicReflectorDish {
 
     fun part1(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
+        val input = parseInput(list)
+
+        for (row in input.indices) {
+            for (column in input[0].indices) {
+                if (input[row][column] == 'O') {
+                    // move up
+                    for (rowUp in row downTo 1) {
+                        if (input[rowUp - 1][column] == '.') {
+                            input[rowUp - 1][column] = 'O'
+                            input[rowUp][column] = '.'
+                        } else {
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        var sum = 0
+        input.dump()
+        for (row in input.indices) {
+            for (column in input[0].indices) {
+                if (input[row][column] == 'O') {
+                    sum += input.size - row
+                }
+            }
+        }
+
+        return sum
     }
+
+    fun Array<Array<Char>>.dump() {
+        for (column in this[0].indices) {
+            for (row in this.indices) {
+                print(this[row][column])
+            }
+            println()
+        }
+    }
+
 
     fun part2(list: List<String>): Int {
         val directions = parseInput(list)
         return 0
     }
 
-    private fun parseInput(list: List<String>): Any {
-        return 0
+    private fun parseInput(list: List<String>): Array<Array<Char>> {
+        return list.map {
+            it.toCharArray().toTypedArray()
+        }.toTypedArray()
     }
 }
 
@@ -29,6 +67,6 @@ fun main() {
     println("part 1: $res1")
     println("part 2: $res2")
 
-    checkResult(res1, 0)
+    checkResult(res1, 112773)
     checkResult(res2, 0)
 }
