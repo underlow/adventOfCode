f477f55b1e87a5aedf16803257e98499d279199f
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2023-12-14 11:58:54 +0100

    2023/day 13 - part 1

--- src/main/kotlin/me/underlow/advent2023/Day13.kt
+++ src/main/kotlin/me/underlow/advent2023/Day13.kt
@@ -4,10 +4,128 @@ import me.underlow.advent2022.checkResult
 import me.underlow.advent2022.readInput
 
 object PointofIncidence {
+    enum class Cell(val n: Int) { Mirror(1), Field(0) }
 
     fun part1(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
+        val mirrors = parseInput(list)
+        return mirrors.map { calc1(it) }.sum()
+
+    }
+
+    fun calc1(mirror: Array<Array<Cell>>): Int {
+        // number of mirrors in columns
+        val colCounts = columnCounts(mirror)
+
+        val rowCounts = rowCounts(mirror)
+
+        val possibleColumns = possibleMirrors(colCounts)
+        val possibleRows = possibleMirrors(rowCounts)
+
+        val mirrorColumns = possibleColumns.filter { columnMirror(it, mirror) }
+        val mirrorRows = possibleRows.filter { rowMirror(it, mirror) }
+
+
+
+        println("pc: $possibleColumns pr: $possibleRows")
+        val column = mirrorColumns.firstOrNull() ?: 0
+        val row = mirrorRows.firstOrNull() ?: 0
+
+        return column + 100 * row
+    }
+
+    fun columnMirror(center: Int, mirror: Array<Array<Cell>>): Boolean {
+        var i = 0
+        var isMirror = true
+//            println("Starting $center position")
+        while (center - i - 1 >= 0 && center + i < mirror[0].size) {
+//                println("Comparing  ${center - i - 1} with ${center + i} (${colCounts[center - i - 1]} with ${colCounts[center + i]})")
+            if (!columnsEqual(mirror, center - i - 1, center + i)) {
+                isMirror = false
+                break
+            }
+            i++
+        }
+        if (isMirror) {
+//                println("Candidate: $center")
+            return isMirror
+        }
+        return isMirror
+    }
+
+    fun rowMirror(center: Int, mirror: Array<Array<Cell>>): Boolean {
+        var i = 0
+        var isMirror = true
+//            println("Starting $center position")
+        while (center - i - 1 >= 0 && center + i < mirror.size) {
+//                println("Comparing  ${center - i - 1} with ${center + i} (${colCounts[center - i - 1]} with ${colCounts[center + i]})")
+            if (!rowsEqual(mirror, center - i - 1, center + i)) {
+                isMirror = false
+                break
+            }
+            i++
+        }
+        if (isMirror) {
+//                println("Candidate: $center")
+            return isMirror
+        }
+        return isMirror
+    }
+
+    private fun columnsEqual(mirror: Array<Array<Cell>>, c1: Int, c2: Int): Boolean {
+        var equal = true
+        for (i in mirror.indices) {
+            if (mirror[i][c1] != mirror[i][c2])
+                equal = false
+        }
+        return equal
+    }
+
+    private fun rowsEqual(mirror: Array<Array<Cell>>, r1: Int, r2: Int): Boolean {
+        var equal = true
+        for (i in mirror[0].indices) {
+            if (mirror[r1][i] != mirror[r2][i])
+                equal = false
+        }
+        return equal
+    }
+
+    fun rowCounts(mirror: Array<Array<Cell>>) =
+        mirror.map { it.sumOf { it.n } }
+
+    fun columnCounts(mirror: Array<Array<Cell>>): MutableList<Int> {
+        val colCounts = MutableList(mirror[0].size) { 0 }
+
+        for (i in mirror[0].indices) {
+            for (j in mirror.indices)
+                colCounts[i] += mirror[j][i].n
+        }
+        return colCounts
+    }
+
+    fun possibleMirrors(
+        colCounts: List<Int>
+    ): List<Int> {
+        // find possible vertical mirror
+        val possibleColumns = mutableListOf<Int>()
+
+        for (center in 1 until colCounts.size) {
+            var i = 0
+            var isMirror = true
+//            println("Starting $center position")
+            while (center - i - 1 >= 0 && center + i < colCounts.size) {
+//                println("Comparing  ${center - i - 1} with ${center + i} (${colCounts[center - i - 1]} with ${colCounts[center + i]})")
+                if (colCounts[center - i - 1] != colCounts[center + i]) {
+                    isMirror = false
+                    break
+                }
+                i++
+            }
+            if (isMirror) {
+//                println("Candidate: $center")
+                possibleColumns.add(center)
+            }
+        }
+        return possibleColumns
     }
 
     fun part2(list: List<String>): Int {
@@ -15,9 +133,24 @@ object PointofIncidence {
         return 0
     }
 
-    private fun parseInput(list: List<String>): Any {
-        return 0
+    fun parseInput(list: List<String>): List<Array<Array<Cell>>> {
+        val groups = mutableListOf<MutableList<String>>()
+        groups.add(mutableListOf())
+        list.forEach { l ->
+            if (l.isEmpty())
+                groups.add(mutableListOf())
+            else
+                groups.last().add(l)
+        }
+
+        return groups.map {
+            it.map {
+                it.toCharArray().map { if (it == '#') Cell.Mirror else Cell.Field }.toTypedArray()
+            }.toTypedArray()
+        }
     }
+
+
 }
 
 
@@ -29,6 +162,6 @@ fun main() {
     println("part 1: $res1")
     println("part 2: $res2")
 
-    checkResult(res1, 0)
+    checkResult(res1, 33195)
     checkResult(res2, 0)
 }
--- src/test/kotlin/me/underlow/advent2023/Day13Test.kt
+++ src/test/kotlin/me/underlow/advent2023/Day13Test.kt
@@ -1,9 +1,27 @@
 package me.underlow.advent2023
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class PointofIncidenceTest {
+    @Test
+    fun possibleTest() {
+        val mirrors = PointofIncidence.parseInput(input.split("\n"))
+        val columnCounts = PointofIncidence.columnCounts(mirrors[0])
+        val rowCounts = PointofIncidence.rowCounts(mirrors[0])
+
+        assertArrayEquals(columnCounts.toTypedArray(), arrayOf(4, 2, 5, 2, 3, 3, 2, 5, 2))
+        assertArrayEquals(rowCounts.toTypedArray(), arrayOf(5, 4, 3, 3, 4, 4, 5))
+
+        val pc = PointofIncidence.possibleMirrors(columnCounts)
+
+        assertArrayEquals(pc.toTypedArray(), arrayOf(5))
+
+        val pr = PointofIncidence.possibleMirrors(rowCounts)
+        assertArrayEquals(pr.toTypedArray(), arrayOf())
+    }
+
     @Test
     fun testPart1() {
         val result = PointofIncidence.part1(input.split("\n"))
