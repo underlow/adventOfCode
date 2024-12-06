696c457ff84fc78ff84101985796c220e4fc4d6f
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-06 09:03:49 +0100

    2024/day 06 - part 1

--- src/main/kotlin/me/underlow/advent2024/Day06.kt
+++ src/main/kotlin/me/underlow/advent2024/Day06.kt
@@ -1,12 +1,66 @@
+import me.underlow.Dir
+import me.underlow.Point
 import me.underlow.advent2022.checkResult
 import me.underlow.advent2022.readInput
 import me.underlow.advent2024.pathPrefix24
+import me.underlow.parseToMap
 
 object GuardGallivant {
 
     fun part1(list: List<String>): Int {
         val directions = parseInput(list)
-        return 0
+
+        var start: Point? = null
+        var dir: Dir? = null
+        for (i in list.indices) {
+            for (j in list[0].indices)
+                when {
+                    list[i][j] == '^' -> {
+                        start = Point(i, j)
+                        dir = Dir.Up
+                    }
+
+                    list[i][j] == '>' -> {
+                        start = Point(i, j)
+                        dir = Dir.Right
+                    }
+
+                    list[i][j] == 'v' -> {
+                        start = Point(i, j)
+                        dir = Dir.Down
+                    }
+
+                    list[i][j] == '<' -> {
+                        start = Point(i, j)
+                        dir = Dir.Left
+                    }
+                }
+        }
+
+        val visited = mutableSetOf<Point>()
+        visited += start!!
+
+        while (start!!.row in list.indices && start.col in list[0].indices) {
+            //make a step
+            val next = start.move(dir!!)
+
+            if (!(next.row in list.indices && next.col in list[0].indices)) {
+                break
+            }
+
+            if (list[next.row][next.col] != '#') {
+                start = next
+                visited += start
+                println("${visited.size} Step to $start")
+                continue
+            }
+
+            dir = dir.rotateRight()
+            println("Rotate to $dir")
+        }
+
+
+        return visited.size
     }
 
     fun part2(list: List<String>): Int {
@@ -14,8 +68,8 @@ object GuardGallivant {
         return 0
     }
 
-    private fun parseInput(list: List<String>): Any {
-        return 0
+    private fun parseInput(list: List<String>): Array<Array<Char>> {
+        return list.parseToMap()
     }
 }
 
@@ -25,7 +79,7 @@ fun main() {
     val res1 = GuardGallivant.part1(input)
     val res2 = GuardGallivant.part2(input)
 
-    checkResult(res1, 0)
+    checkResult(res1, 4722)
     checkResult(res2, 0)
 
     println(res1)
