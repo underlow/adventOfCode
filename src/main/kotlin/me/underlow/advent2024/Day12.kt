155da609f5c0b36df714f57f0c142b7a15f9809d
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-12 08:44:20 +0100

    2024/day 12 - part 1

--- src/main/kotlin/me/underlow/advent2024/Day12.kt
+++ src/main/kotlin/me/underlow/advent2024/Day12.kt
@@ -1,14 +1,80 @@
 package me.underlow.advent2024
 
+import me.underlow.Point
 import me.underlow.advent2022.checkResult
 import me.underlow.advent2022.readInput
+import me.underlow.isPointInside
 import me.underlow.parseToMap
 
 object GardenGroups {
 
+    data class Plant(val point: Point, val char: Char, val groupId: Int, val fence: List<Point>) {
+        fun perimeter(field: Array<Array<Plant?>>) =
+            fence.count { !field.isPointInside(it) } + fence.count { field.isPointInside(it) && field[it.row][it.col]!!.char != this.char }
+    }
+
+
     fun part1(list: List<String>): Int {
         val charField = list.parseToMap()
-        return 0
+
+        val field = Array<Array<Plant?>>(charField.size) {
+            Array(charField[0].size) { null }
+        }
+        var gId = 0
+        // find out groups
+        for (i in charField.indices) {
+            for (j in charField[0].indices) {
+                if (field[i][j] != null)
+                    continue
+
+                val groupPoints = findPoints(charField, i, j, emptySet())
+                groupPoints.forEach { p ->
+                    field[p.row][p.col] =
+                        Plant(
+                            p,
+                            charField[p.row][p.col],
+                            gId,
+                            p.around()
+                        )
+                }
+                gId++
+
+            }
+        }
+
+        val grouped = mutableMapOf<Int, List<Plant>>()
+
+        for (i in field.indices) {
+            for (j in field[0].indices) {
+                val p = field[i][j]!!
+                grouped[p.groupId] = grouped.getOrElse(p.groupId) { emptyList() } + listOf(p)
+            }
+        }
+
+        val s = grouped.entries.map { (k, points) ->
+            val perimeter = points.sumOf { it.perimeter(field) }
+            val area = points.size
+            return@map perimeter * area
+        }
+
+
+        return s.sum()
+    }
+
+    private fun findPoints(field: Array<Array<Char>>, i: Int, j: Int, visited: Set<Point>): Set<Point> {
+        val p = Point(i, j)
+
+        if (p in visited)
+            return emptySet()
+
+        val c = field[i][j]
+
+        val candidates = p.around()
+            .filter { field.isPointInside(it) }
+            .filter { field[it.row][it.col] == c }
+//            .filter { it !in visited }
+        return (candidates + listOf(p) + candidates.map { findPoints(field, it.row, it.col, visited + it) }
+            .flatten()).toSet()
     }
 
     fun part2(list: List<String>): Int {
@@ -25,7 +91,7 @@ fun main() {
     val res1 = GardenGroups.part1(input)
     val res2 = GardenGroups.part2(input)
 
-    checkResult(res1, 0)
+    checkResult(res1, 0) // 34262 low
     checkResult(res2, 0)
 
     println(res1)
