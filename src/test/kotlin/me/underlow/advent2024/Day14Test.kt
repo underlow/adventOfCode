386fb783fa197e309d75b793a507127430fe6189
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-14 08:37:25 +0100

    2024/day 14 - part 1

---
+++ src/test/kotlin/me/underlow/advent2024/Day14Test.kt
@@ -0,0 +1,32 @@
+import me.underlow.advent2024.RestroomRedoubt
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class RestroomRedoubtTest {
+    @Test
+    fun testPart1() {
+        val result = RestroomRedoubt.part1(input.split("\n"))
+        assertEquals(12, result)
+    }
+
+    @Test
+    fun testPart2() {
+        val result = RestroomRedoubt.part2(input.split("\n"))
+        assertEquals(0, result)
+    }
+}
+
+private val input = """
+p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+""".trimIndent()
