170db18348ecc7498b90ff8b9bc74f98b0d88c78
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-16 09:40:57 +0100

    2024/day 16 - part 1

--- src/main/kotlin/me/underlow/advent2024/Day16.kt
+++ src/main/kotlin/me/underlow/advent2024/Day16.kt
@@ -82,9 +82,76 @@ object ReindeerMaze {
     }
 
     fun part2(list: List<String>): Int {
-        val field = list.parseToMap()
+        val field: Array<Array<Char>> = list.parseToMap()
 
-        return 0
+        var start = Point(0, 0)
+        for (i in field.indices)
+            for (j in field[0].indices)
+                if (field[i][j] == 'S')
+                    start = Point(i, j)
+
+
+        val visited = mutableMapOf<Key, Int>()
+        var result = Int.MAX_VALUE
+        var resultPath = mutableSetOf<Point>()
+        val queue = mutableListOf<Reindeer>()
+        queue += Reindeer(start, Dir.Right, 0, emptyList())
+
+        while (queue.isNotEmpty()) {
+            val r = queue.removeLast()
+            if (field.get(r.p) == 'E') {
+                if (r.points < result) {
+                    println("New result: ${r.points}")
+                    result = r.points
+                    resultPath = r.path.toMutableSet()
+                    continue
+                } else if (r.points == result) {
+                    resultPath += r.path.toMutableSet()
+                    continue
+                }
+            }
+
+            val v = visited[r.toKey()]
+            if (v != null) {
+                if (v < r.points) {
+//                    println("Cache hit")
+                    continue
+                }
+            }
+
+            visited[r.toKey()] = r.points
+
+            // forward
+            val pForward = r.p.move(r.dir)
+            if (field.isPointInside(pForward) && field.get(pForward) != '#') {
+                val r = Reindeer(pForward, r.dir, r.points + 1, r.path + pForward)
+                queue += r
+            }
+            // clockwise
+            val pClockwise = r.p.move(r.dir.rotateLeft())
+            if (field.isPointInside(pClockwise) && field.get(pClockwise) != '#') {
+                val r = Reindeer(pClockwise, r.dir.rotateLeft(), r.points + 1001, r.path + pClockwise)
+                queue += r
+            }
+            // counterclockwise
+            val pCounterClockwise = r.p.move(r.dir.rotateRight())
+            if (field.isPointInside(pCounterClockwise) && field.get(pCounterClockwise) != '#') {
+                val r = Reindeer(pCounterClockwise, r.dir.rotateRight(), r.points + 1001, r.path + pCounterClockwise)
+                queue += r
+            }
+        }
+
+        println("Path is: $resultPath")
+
+        resultPath.forEach {
+            if (field.get(it) == '#')
+                error("www")
+            field[it] = '*'
+        }
+
+        field.dumpWithAxis()
+
+        return resultPath.size
     }
 
 }
@@ -95,7 +162,7 @@ fun main() {
     val res1 = ReindeerMaze.part1(input)
     val res2 = ReindeerMaze.part2(input)
 
-    checkResult(res1, 0) // 105508 high
+    checkResult(res1, 103512) // 105508 high
     checkResult(res2, 0)
 
     println(res1)
