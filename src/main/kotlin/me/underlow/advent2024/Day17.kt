578a3ac61d8f4757efc44e9e768a7dd02a5dd118
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-17 16:12:54 +0100

    2023/day 17 - part 2

--- src/main/kotlin/me/underlow/advent2024/Day17.kt
+++ src/main/kotlin/me/underlow/advent2024/Day17.kt
@@ -1,93 +1,108 @@
 package me.underlow.advent2024
 
-import me.underlow.advent2022.checkResult
 import me.underlow.advent2022.readInput
 import java.lang.Math.pow
 import kotlin.math.truncate
 
 object ChronospatialComputer {
 
-    data class Programm(val rA: Int, val rB: Int, val rC: Int, val instructions: List<Int>)
+    var debug = true
+    fun log(s: String) {
+        if (debug)
+            println(s)
+    }
+
+    data class Programm(val rA: Long, val rB: Long, val rC: Long, val instructions: List<Int>)
 
     fun part1(list: List<String>): String {
         val program = parseInput(list)
 
-        return execute(program)
+        return execute(program).joinToString(",")
     }
 
-    private fun execute(program: Programm): String {
+    private fun execute(program: Programm): List<Int> {
         val out = mutableListOf<Int>()
 
         var registerA = program.rA
         var registerB = program.rB
         var registerC = program.rC
 
-        var pointer = 0
+        var pointer = 0L
 
-        fun Int.comboOperand() = when (this) {
-            in 0..3 -> this
+        fun Int.comboOperand(): Long = when (this) {
+            in 0..3 -> this.toLong()
             4 -> registerA
             5 -> registerB
             6 -> registerC
             else -> error("combo operand")
         }
 
-        fun Int.literalOperand() = this
+        fun Int.literalOperand() = this.toLong()
 
         while (pointer < program.instructions.size) {
-            val operand = program.instructions[pointer + 1]
-            when (program.instructions[pointer]) {
+            val operand = program.instructions[pointer.toInt() + 1]
+            when (program.instructions[pointer.toInt()]) {
                 // adv
                 0 -> {
                     val numerator = registerA
                     val denominator = pow(2.0, operand.comboOperand().toDouble()).toInt()
                     val result = truncate(numerator / denominator.toDouble()).toInt()
-                    registerA = result
+                    log("adv( >> 3): A:=$result")
+                    registerA = result.toLong()
                     pointer += 2
                 }
                 //bxl
                 1 -> {
                     val result = registerB xor operand.literalOperand()
                     registerB = result
+                    log("bxl (xor): B:=$result")
                     pointer += 2
                 }
                 // bst
                 2 -> {
                     registerB = operand.comboOperand() % 8
+                    log("bst (last 3): B:=$registerB")
                     pointer += 2
                 }
                 //                jnz
                 3 -> {
-                    if (registerA != 0) {
+                    if (registerA != 0L) {
                         pointer = operand.literalOperand()
+                        log("jnz: $pointer")
+                        log("A: $registerA, B: $registerB, C: $registerC")
                     } else {
                         pointer += 2
+                        log("jnz: nothing")
                     }
                 }
                 //bxc
                 4 -> {
                     registerB = registerB xor registerC
+                    log("bxc (xor): B:=$registerB")
                     pointer += 2
                 }
                 // out
                 5 -> {
-                    out += operand.comboOperand() % 8
+                    out += (operand.comboOperand() % 8L).toInt()
+                    log("out: ${operand.comboOperand() % 8}")
                     pointer += 2
                 }
                 //bdv
                 6 -> {
                     val numerator = registerA
-                    val denominator = pow(2.0, operand.comboOperand().toDouble()).toInt()
-                    val result = truncate(numerator / denominator.toDouble()).toInt()
+                    val denominator = pow(2.0, operand.comboOperand().toDouble()).toLong()
+                    val result = truncate(numerator / denominator.toDouble()).toLong()
                     registerB = result
+                    log("bdv (>>3): B:=$registerB")
                     pointer += 2
                 }
-                //bdc
+                //cvd
                 7 -> {
                     val numerator = registerA
-                    val denominator = pow(2.0, operand.comboOperand().toDouble()).toInt()
-                    val result = truncate(numerator / denominator.toDouble()).toInt()
+                    val denominator = pow(2.0, operand.comboOperand().toDouble()).toLong()
+                    val result = truncate(numerator / denominator.toDouble()).toLong()
                     registerC = result
+                    log("cvd (>>): C:=$registerC")
                     pointer += 2
                 }
 
@@ -95,18 +110,61 @@ object ChronospatialComputer {
             }
         }
 
-        return out.joinToString(",")
+        return out
     }
 
-    fun part2(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
+    fun part2(list: List<String>): Long {
+        println("PART2")
+        val program = parseInput(list)
+
+        var res = 0L
+        for (firstNum in 0..7) {
+            for (inst in program.instructions.indices.reversed()) {
+                if (inst == program.instructions.size - 1)
+                    continue
+                for (num in 0..7) {
+                    val num2 = 7
+                    val execute = execute(program.copy(rA = (res shl 3) + num2))
+                    if (program.instructions[inst] == execute.first()) {
+                        if (num == 0 && inst == program.instructions.size - 1) {
+                            continue
+                        }
+                        res = (res shl 3) + num
+                        println("Found for $inst: ${program.instructions[inst]}, result: $res")
+                        break
+                    }
+                }
+
+            }
+
+            // lets check
+
+            val e = execute(program.copy(rA = res))
+            if (e != program.instructions) {
+                println("Not correct $res")
+            } else {
+                println("Great: $res")
+            }
+        }
+
+        return res
+
+
+        for (i in program.rA..program.rA) {
+//            if (i % 1000000 == 0)
+//                println("i == $i")
+            val result = execute(program.copy(rA = i))
+            if (result == program.instructions)
+                return i
+            println("$i: $result")
+        }
+        return -1
     }
 
     private fun parseInput(list: List<String>): Programm {
-        val rA = list[0].removePrefix("Register A: ").toInt()
-        val rB = list[1].removePrefix("Register B: ").toInt()
-        val rC = list[2].removePrefix("Register C: ").toInt()
+        val rA = list[0].removePrefix("Register A: ").toLong()
+        val rB = list[1].removePrefix("Register B: ").toLong()
+        val rC = list[2].removePrefix("Register C: ").toLong()
 
         val instructions = list[4].removePrefix("Program: ").split(",").map { it.trim().toInt() }
 
@@ -116,13 +174,14 @@ object ChronospatialComputer {
 
 
 fun main() {
+    ChronospatialComputer.debug = false
     val input = readInput("$pathPrefix24/day17.txt")
-    val res1 = ChronospatialComputer.part1(input)
+//    val res1 = ChronospatialComputer.part1(input)
     val res2 = ChronospatialComputer.part2(input)
 
-    checkResult(res1, 0)
-    checkResult(res2, 0)
+//    checkResult(res1, "6,7,5,2,1,3,5,1,7")
+//    checkResult(res2, 0)
 
-    println(res1)
+//    println(res1)
     println(res2)
 }
--- src/test/kotlin/me/underlow/advent2024/Day17Test.kt
+++ src/test/kotlin/me/underlow/advent2024/Day17Test.kt
@@ -11,8 +11,15 @@ class ChronospatialComputerTest {
 
     @Test
     fun testPart2() {
-        val result = ChronospatialComputer.part2(input.split("\n"))
-        assertEquals(0, result)
+        ChronospatialComputer.debug = false
+        val result = ChronospatialComputer.part2(input2.split("\n"))
+        assertEquals(117440, result)
+    }
+
+    @Test
+    fun testPart21() {
+        val result = ChronospatialComputer.part1(input21.split("\n"))
+        assertEquals("0,3,5,4,3,0", result)
     }
 }
 
@@ -22,4 +29,19 @@ private val input = """
  Register C: 0
 
  Program: 0,1,5,4,3,0
+""".trimIndent()
+
+private val input2 = """
+    Register A: 2024
+    Register B: 0
+    Register C: 0
+
+    Program: 0,3,5,4,3,0
+""".trimIndent()
+private val input21 = """
+    Register A: 117440
+    Register B: 0
+    Register C: 0
+
+    Program: 0,3,5,4,3,0
 """.trimIndent()
