a1081dbdb688cad6d7c5b220e955e044c67b8a5f
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-19 07:21:08 +0100

    2023/day 19 - part 1

---
+++ src/test/kotlin/me/underlow/advent2024/Day19Test.kt
@@ -0,0 +1,30 @@
+import me.underlow.advent2024.LinenLayout
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LinenLayoutTest {
+    @Test
+    fun testPart1() {
+        val result = LinenLayout.part1(input.split("\n"))
+        assertEquals(6, result)
+    }
+
+    @Test
+    fun testPart2() {
+        val result = LinenLayout.part2(input.split("\n"))
+        assertEquals(0, result)
+    }
+}
+
+private val input = """
+ r, wr, b, g, bwu, rb, gb, br
+
+ brwrr
+ bggr
+ gbbr
+ rrbgbr
+ ubwu
+ bwurrg
+ brgr
+ bbrgwb
+""".trimIndent()
