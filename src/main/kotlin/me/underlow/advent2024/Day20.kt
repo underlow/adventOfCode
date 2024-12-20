2a008d77fa59dcf62261a18a0dc8b48cc74d7547
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-20 09:38:58 +0100

    2023/day 20 - part 1

--- src/main/kotlin/me/underlow/advent2024/Day20.kt
+++ src/main/kotlin/me/underlow/advent2024/Day20.kt
@@ -13,7 +13,7 @@ object RaceCondition {
         val start = field.findFirst('S')
         val finish = field.findFirst('E')
 
-        val baseline = field.findShortestPath(start, finish)
+        val baseline = field.findShortestPathLength(start, finish)
 
         // find all walls
         val walls = mutableListOf<Point>()
@@ -30,34 +30,66 @@ object RaceCondition {
         // for each pair of close walls calculate path and store it
 
         for (p1 in walls) {
-            val p2List = p1.around().filter { field.isPointInside(it) }.filter { field.get(it) == '#' }
+            val p2List = p1.around().filter { field.isPointInside(it) }.filter { field.get(it) != 'S' }
 
             for (p2 in p2List) {
 //                if (p1 == Point(1,8) && p2 == Point(1, 9)){
 //                    field.dumpWithAxis()
 //                }
-                if (cheatPairs[CheatPair(p1, p2)] == null) {
-                    field[p1] = '.'
-                    field[p2] = '.'
+//                if (cheatPairs[CheatPair(p1, p2)] == null) {
+                field[p1] = '.'
+                val p1Old = field.get(p2)
+//                field[p2] = '.'
 //                    if (p1 == Point(1,8) && p2 == Point(1, 9)){
 //                        field.dumpWithAxis()
 //                    }
-                    val path = field.findShortestPath(start, finish)
-                    cheatPairs[CheatPair(p1, p2)] = path
-                    cheatPairs[CheatPair(p2, p1)] = path
-                    field[p1] = '#'
-                    field[p2] = '#'
-//                    field.dumpWithAxis()
+                val path = listOf(start) + field.findShortestPath(start, finish)
+                field[p1] = '#'
+//                field[p2] = p1Old
+                // let's find cheat points
+
+                val m1 = path.indexOfFirst { it == p1 }
+//                val m2 = path.indexOfFirst { it == p2 }
+//                if (m1 == -1 && m2 == -1) {
+//                    continue
+//                }
+//                if (m2 == 0){
+//                    println()
+//                }
+                when {
+                    m1 == -1 /*&& m2 == -1*/ -> continue
+//                    m1 == -1 -> {
+//                        cheatPairs[CheatPair(path[m2 - 1], path[m2])] = path.size
+//
+//                    }
+//                    m2 == -1 -> {
+//                        cheatPairs[CheatPair(path[m1 - 1], path[m1])] = path.size
+//
+//                    }
+//                    m1 < m2 -> {
+//                        cheatPairs[CheatPair(path[m1 - 1], path[m1])] = path.size
+//                    }
+                    else -> {
+                        cheatPairs[CheatPair(path[m1 - 1], path[m1])] = path.size
+                    }
                 }
+
+//                    cheatPairs[CheatPair(p1, p2)] = path
+//                    cheatPairs[CheatPair(p2, p1)] = path
+
+//                    field.dumpWithAxis()
+//                }
             }
         }
 
         val ss = cheatPairs.entries
             .filter { it.value < baseline }
+        val ss2 = cheatPairs.entries
+            .filter { it.value == 21 }
 
         return cheatPairs.entries
             .filter { it.value < baseline }
-            .count() / 2
+            .count()
     }
 
     fun part2(list: List<String>): Int {
