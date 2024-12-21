1f63080ed2be4dcd0a6d489127a24b17e61c111f
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-21 08:08:25 +0100

    2024/day 21 - part 1

--- src/test/kotlin/me/underlow/advent2024/Day21Test.kt
+++ src/test/kotlin/me/underlow/advent2024/Day21Test.kt
@@ -6,7 +6,7 @@ class KeypadConundrumTest {
     @Test
     fun testPart1() {
         val result = KeypadConundrum.part1(input.split("\n"))
-        assertEquals(0, result)
+     assertEquals(126384, result)
     }
 
     @Test
@@ -17,5 +17,9 @@ class KeypadConundrumTest {
 }
 
 private val input = """
- 
+ 029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+ 980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
+ 179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+ 456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
+ 379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
 """.trimIndent()
