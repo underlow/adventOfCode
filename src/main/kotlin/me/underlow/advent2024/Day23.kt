14831c8a78c1d1f47573985063cf6ec16497ca3d
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-23 09:13:55 +0100

    2024/day 23 - part 1

--- src/main/kotlin/me/underlow/advent2024/Day23.kt
+++ src/main/kotlin/me/underlow/advent2024/Day23.kt
@@ -5,9 +5,29 @@ import me.underlow.advent2022.readInput
 
 object LANParty {
 
+    data class Node(val id: String, val nodes: MutableList<Node>)
+
+    // looking for exactly 3 node cycle
+    data class Cycle(val id1: String, val id2: String, val id3: String) {
+        fun hasT() = id1.startsWith("t") || id2.startsWith("t") || id3.startsWith("t")
+    }
+
     fun part1(list: List<String>): Int {
-        val directions = parseInput(list)
-        return 0
+        val graph = parseInput(list)
+
+        val cycles = mutableSetOf<Cycle>()
+
+        for (node in graph) {
+            val c = findCycle(node, graph)
+            if (c != null)
+                cycles += c
+        }
+
+        return cycles.filter { it.hasT() }.count()
+    }
+
+    fun findCycle(node: Node, graph: List<Node>): Cycle? {
+        return null
     }
 
     fun part2(list: List<String>): Int {
@@ -15,8 +35,20 @@ object LANParty {
         return 0
     }
 
-    private fun parseInput(list: List<String>): Any {
-        return 0
+    private fun parseInput(list: List<String>): List<Node> {
+        val graph = mutableListOf<Node>()
+        list.forEach { s ->
+            val s1 = s.split('-')[0]
+            val s2 = s.split('-')[1]
+            if (s1 == s2) {
+                error("slkdfjhgkr")
+            }
+            val n1 = graph.firstOrNull { it.id == s1 } ?: Node(s1, mutableListOf())
+            val n2 = graph.firstOrNull { it.id == s2 } ?: Node(s2, mutableListOf())
+            n1.nodes.add(n2)
+            n2.nodes.add(n1)
+        }
+        return graph
     }
 }
 
