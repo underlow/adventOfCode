cdfba7af4a2c259b6e045aff065f1074d0bc2360
Sergey Mikhaylov <sergei.mikhailov@gmail.com>
2024-12-23 19:36:58 +0100

    2024/day 23 - part 2

--- src/main/kotlin/me/underlow/advent2024/Day23.kt
+++ src/main/kotlin/me/underlow/advent2024/Day23.kt
@@ -2,10 +2,12 @@ package me.underlow.advent2024
 
 import me.underlow.advent2022.checkResult
 import me.underlow.advent2022.readInput
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 object LANParty {
 
-    data class Node(val id: String, val nodes: MutableList<Node>) {
+    data class Node(val id: String, val nodes: MutableList<Node>, val nodeIds: MutableSet<String>) {
         override fun toString(): String {
             return "Node(id='$id', nodes=${nodes.map { it.id }})"
         }
@@ -45,39 +47,18 @@ object LANParty {
         return result
     }
 
-    data class Pointer(val start: Node, val count: Int, val visited: List<String>)
-
-    fun findCycleT(node: Node, graph: List<Node>): List<String> {
-        var result = mutableListOf<String>()
-        val first = node
-
-        node.nodes.forEach { second ->
-            val cycle = runPointer(Pointer(first, 1, listOf(first.id)), graph, first)
-            if (cycle.isNotEmpty())
-                result = cycle.toMutableList()
-        }
-
-        return result
-    }
-
-    private fun runPointer(pointer: Pointer, graph: List<Node>, current: Node): List<String> {
-//        if (pointer.count == size) {
+    private fun runPointer(visited: Set<String>, current: Node): List<String> {
         var result = emptyList<String>()
-        // if next is the start
-//        if (pointer.start == current && pointer.count > 1)
-//            return pointer.visited/*+ current.id*/
-
-//            return emptyList()
-//        }
 
         // if not found and not in the end of the cycle
-        val notInVisited = current.nodes.filter { it.id !in pointer.visited }
+        val notInVisited = current.nodes.filter { it.id !in visited }
         for (node in notInVisited) {
-            // if we can get from node to any already visited
-            val lll = node.nodes.map { it.id }
-            if (pointer.visited.all { it in lll }) {
-                val newPointer = pointer.copy(count = pointer.count + 1, visited = pointer.visited + node.id)
-                val found = runPointer(newPointer, graph, node)
+            if (node.id in result)
+                continue
+            // if we can get from this node to any already visited
+            if (visited.all { it in node.nodeIds }) {
+                val found = runPointer(visited + node.id, node)
+
                 if (found.isNotEmpty() && result.size <= found.size) {
                     result = found + node.id
                 } else {
@@ -98,22 +79,14 @@ object LANParty {
 
         val result = emptyList<String>()
 
-//        for (i in 4..graph.size) {
         for (node in graph) {
-//                if (node.id != "co")
-//                    continue
-            val c = listOf(node.id) + runPointer(Pointer(node, 1, listOf(node.id)), graph, node)
-//            val c = findCycleT(node, graph)
+            val c = listOf(node.id) + runPointer(setOf(node.id), node)
 
             if (c.isNotEmpty() && cycle.size < c.size) {
-                println("Cycle for $node is $c")
+                println("Cycle for $node is ${c.sortedBy { it }}")
                 cycle = c
 
-//                    if (i == 3) {
-//                        println("For 3 found $c")
-//                    }
             }
-//            }
         }
 
         return cycle.sortedBy { it }.joinToString(",")
@@ -127,25 +100,37 @@ object LANParty {
             if (s1 == s2) {
                 error("slkdfjhgkr")
             }
-            val n1 = graph.firstOrNull { it.id == s1 } ?: Node(s1, mutableListOf()).also { graph.add(it) }
-            val n2 = graph.firstOrNull { it.id == s2 } ?: Node(s2, mutableListOf()).also { graph.add(it) }
+            val n1 =
+                graph.firstOrNull { it.id == s1 } ?: Node(s1, mutableListOf(), mutableSetOf()).also { graph.add(it) }
+            val n2 =
+                graph.firstOrNull { it.id == s2 } ?: Node(s2, mutableListOf(), mutableSetOf()).also { graph.add(it) }
             n1.nodes.add(n2)
             n2.nodes.add(n1)
 
+        }
+        graph.forEach {
+            it.nodeIds += it.nodes.map { it.id }
         }
         return graph
     }
 }
 
 
+@OptIn(ExperimentalTime::class)
 fun main() {
     val input = readInput("$pathPrefix24/day23.txt")
-    val res1 = LANParty.part1(input)
-    val res2 = LANParty.part2(input)
+    val time = measureTime {
+        val res1 = LANParty.part1(input)
+        checkResult(res1, 1046)
+        println(res1)
+    }
+    println("Time: $time")
 
-    checkResult(res1, 1046)
-    checkResult(res2, 0)
+    val time2 = measureTime {
+        val res2 = LANParty.part2(input)
+        checkResult(res2, "de,id,ke,ls,po,sn,tf,tl,tm,uj,un,xw,yz")
+        println(res2)
+    }
+    println("Time: $time2")
 
-    println(res1)
-    println(res2)
 }
